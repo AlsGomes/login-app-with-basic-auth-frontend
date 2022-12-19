3aa7ed7242555544df8e0854de9df4900fc3283c
AlsGomes <als_08.net@hotmail.com>
2022-12-19 20:02:02 -0300

    Login page created

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -7,7 +13,12 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
+    InputTextModule,
+    PasswordModule,
+    DividerModule,
+    ButtonModule
   ]
 })
 export class LoginModule { }
